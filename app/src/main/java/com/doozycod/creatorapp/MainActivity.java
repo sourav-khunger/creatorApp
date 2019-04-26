991d9b91577237686765eeb76bb5a01d6bbd4e6e
Sourav Khunger <khungersourav30@gmail.com>
2019-04-26 12:21:40 +0530

    Initial commit

---
+++ app/src/main/java/com/doozycod/creatorapp/MainActivity.java
@@ -0,0 +1,112 @@
+package com.doozycod.creatorapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button addtextButton, addEditText, addRadioButton, addCheckbox, addButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addEditText = findViewById(R.id.edittext);
+        addtextButton = findViewById(R.id.textView);
+        addRadioButton = findViewById(R.id.RadioButtons);
+        addCheckbox = findViewById(R.id.checkbox);
+        addButton = findViewById(R.id.submit_btn);
+
+        addEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addEditText();
+            }
+        });
+        addtextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTextView();
+            }
+        });
+        addRadioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addRadioGroup();
+            }
+        });
+        addCheckbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addCheckbox();
+            }
+        });
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addButton();
+            }
+        });
+    }
+
+
+    private void addEditText() {
+        LinearLayout layout = findViewById(R.id.edittextContainer);
+        EditText editText = new EditText(this);
+        editText.setHint("Text is here");
+        editText.setId(editText.generateViewId());
+        Log.e("editText",editText.generateViewId()+"");
+
+
+        editText.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        layout.addView(editText);
+    }
+
+    private void addTextView() {
+        LinearLayout layout = findViewById(R.id.edittextContainer);
+        TextView tv = new TextView(this);
+        tv.setText("Text is here");
+        tv.setId(tv.generateViewId());
+        Log.e("TEXT",tv.generateViewId()+"");
+
+        tv.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        layout.addView(tv);
+    }
+
+    private void addRadioGroup() {
+        LinearLayout layout = findViewById(R.id.edittextContainer);
+        RadioGroup rg = new RadioGroup(this);
+        rg.setId(rg.generateViewId());
+        rg.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        layout.addView(rg);
+    }
+
+    private void addCheckbox() {
+        LinearLayout layout = findViewById(R.id.edittextContainer);
+        CheckBox cb = new CheckBox(this);
+        cb.setId(cb.generateViewId());
+        cb.setText("Checkbox");
+        cb.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        layout.addView(cb);
+    }
+
+    private void addButton() {
+        LinearLayout layout = findViewById(R.id.edittextContainer);
+        Button button = new Button(this);
+        button.setText("Submit");
+        button.setId(button.generateViewId() + 1);
+        button.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        layout.addView(button);
+    }
+
+
+}
